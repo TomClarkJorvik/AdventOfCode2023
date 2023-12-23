ee599a16559daa579e88af17e2a342a020d4c88f
TomClarkYorkie <twangletoo@gmail.com>
2023-12-23 13:28:50 +0000

    Puzzle Two
    Changed how a line is searched over to find all possible digits in the line and add it to an array.
    Added recursive word searcher to see if a character is the start of a word.

--- MyProgram.py
+++ MyProgram.py
@@ -1,14 +1,44 @@
 
-def findCalibrationFromLine(line):
-    firstDigit = ""
-    secondDigit = ""
-    for character in line:
-        if character.isdigit():
-            if firstDigit == "":
-                firstDigit = character
-                secondDigit = character
+possibleDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+digitDict = {"one":"1","two":"2","three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def parseWordRecursion(line, lineIndex, word, wordIndex):
+    if (wordIndex < len(word)) and (lineIndex < len(line)):
+        if line[lineIndex] == word[wordIndex]:
+            if (wordIndex == (len(word)-1)):
+                return True, word
             else:
-                secondDigit = character
+                return parseWordRecursion(line, lineIndex+1, word, wordIndex+1)
+    return False, ""
+
+def parseWord(line, lineIndex):
+    for digit in possibleDigits:
+        if line[lineIndex] == digit[0]:
+            isDigit, digit = parseWordRecursion(line, lineIndex+1, digit, 1)
+            if isDigit:
+                # Only return if it was a digit, otherwise keep searching
+                return isDigit, digit
+    return False, ""
+
+def findListOfDigits(line):
+    # Returns the list of digits in the order they appear.
+    # Array is a list of strings.
+    retArray = []            
+    for i in range(len(line)):
+        character = line[i]
+        if character.isdigit():
+            retArray.append(character)
+        else:
+            isDigit, digit = parseWord(line, i)
+            if isDigit:
+                retArray.append(digitDict[digit])
+    return retArray
+
+def findCalibrationFromLine(line):
+    listOfDigits = findListOfDigits(line)
+    
+    firstDigit = listOfDigits[0]
+    secondDigit = listOfDigits[len(listOfDigits)-1]
 
     retVal = int(firstDigit + secondDigit)
     return retVal
@@ -19,5 +49,6 @@ for line in file:
     line = line.strip()
     lineCalibrationVal = findCalibrationFromLine(line)
     runningCalibrationVal += lineCalibrationVal
+
 file.close()
 print(runningCalibrationVal)
